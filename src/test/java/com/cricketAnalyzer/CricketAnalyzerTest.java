87b4063d4266820e93ca46d63e369297fc046fb1
jitesh <jitesh0dubey@gmail.com>
2020-03-07 18:24:13 +0530

    ADDED:Welcome Message

---
+++ src/test/java/com/cricketAnalyzer/CricketAnalyzerTest.java
@@ -0,0 +1,11 @@
+package com.cricketAnalyzer;
+
+import org.junit.Test;
+
+public class CricketAnalyzerTest {
+
+    @Test
+    public void givenIPLRunData_WhenProper_ShouldReturnSortedAvarageRun() {
+        System.out.println("welcome of ipl analyzer");
+    }
+}
